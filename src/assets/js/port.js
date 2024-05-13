13f5b34d046977226fa68f6432a2ee687e3f88bc
Syeseong <sy051398@naver.com>
2024-05-13 16:26:27 +0900

    feat : 24-05-13 - 포트폴리오 영역

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,3 +1,22 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/src/ScrollTrigger";
+
 export function port() {
-    console.log(port)
+    gsap.registerPlugin(ScrollTrigger);
+    // 사용한다고 선언
+
+    const horSection = gsap.utils.toArray(".port__item")
+
+    gsap.to(horSection, {
+        xPercent: -120 * (horSection.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#portfolio",
+            start: "top 30px",
+            end: "+=3000",
+            pin: true,
+            scrub: 1,
+            markers: false
+        }
+    })
 }
