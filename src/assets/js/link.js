7aed9bf7afa1f229ae45dc954da36efb47db2729
Syeseong <sy051398@naver.com>
2024-05-14 17:29:48 +0900

    feat : 24-05-14 - 마무리 완료

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -1,3 +1,14 @@
 export function link() {
-    console.log(link)
+    document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href");
+            const targetElemnet = document.querySelector(targetId);
+
+            if (targetElemnet) {
+                targetElemnet.scrollIntoView({ behavior: "smooth" });
+            }
+        })
+    })
 }
--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,3 +1,16 @@
+import Lenis from 'lenis';
+
 export function smooth() {
-    console.log(smooth)
+    const lenis = new Lenis()
+
+    lenis.on("scroll", (e) => {
+        console.log(e)
+    })
+
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
 }
